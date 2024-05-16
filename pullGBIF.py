4ea21285fbbeee75c9ee8a628aca06c6a6a8da89
krsherrill <kirk_Sherrill@nps.gov>
2024-05-16 15:25:30 -0600

    Initial Push to GitHub

---
+++ pullGBIF.py
@@ -0,0 +1,21 @@
+"""
+pullGBIF.py
+Script will pull species occurences data from the GBIF https://www.gbif.org/ repository via the GBIF API and the
+pygbif client - https://www.gbif.org/tool/OlyoYyRbKCSCkMKIi4oIT/pygbif-gbif-python-client
+
+First lookup the GBIF Taxon Code via the 'Species' code
+
+occurrences API
+
+Input:
+    speciesLookup - Speices Lookup Table with defined Taxon to be pulled from the
+
+Output:
+
+
+Python Environment: PCM_VegClimateVA - Python 3.11
+
+Date Developed - May 2024
+Created By - Kirk Sherrill - Data Scientist/Manager San Francisco Bay Area Network Inventory and Monitoring
+"""
+
