558b1f96cca0748a9617c6ddf646b98ba395b888
krsherrill <kirk_Sherrill@nps.gov>
2024-05-29 17:28:45 -0600

    Update extractSummarizeAETDeficit.py
    Additional framing out updates

--- extractSummarizeAETDeficit.py
+++ extractSummarizeAETDeficit.py
@@ -26,13 +26,26 @@ import session_info
 import traceback
 from datetime import datetime
 
-#File (Excel/CSV) with vegtation taxon to be idenfities
+#Excel file with the monitoring Locations
 monitoringLoc = r'C:\Users\KSherrill\OneDrive - DOI\SFAN\Climate\VulnerabilityAssessment\PCM\NAWMACover\PCM_NAWMA_Vegetation_ClimateVA_20240517.xlsx'
+#Monitoring Locations Dictionary with IDField, Latitude, Longitude and Vegation Type field definitions.
+monitoringLocDic = {'IDField': 'Name', 'Latitude': 'Latitude', 'Longitude': 'Longitude', 'vegType': 'SiteType'}
+
+#Excel file with the GBIF Locations
+monitoringLoc = r'C:\Users\KSherrill\OneDrive - DOI\SFAN\Climate\VulnerabilityAssessment\GBIF\PCM_NAWMA_TopTwo_GBIF_Occurrences_20240521.csv'
+#Monitoring Locations Dictionary with IDField, Latitude, Longitude and Vegation Type field definitions.
+gbifLocDic = {'IDField': 'key', 'Latitude': 'decimalLatitude', 'Longitude': 'decimalLongitude',
+              'vegType': 'VegCode'}
+
+#List or Dictionary of WB Rasters to process
+wbList = []
+
+
 
 
 # Output Name, OutDir, and Workspace
-outName = 'PCM_NAWMA_TopTwo_GBIF'  # Output name for excel file and logile
-outDir = r'C:\Users\KSherrill\OneDrive - DOI\SFAN\Climate\VulnerabilityAssessment\GBIF'  # Directory Output Location
+outName = 'PCM_ExtractSummarize_AETDeficit'  # Output name for excel file and logile
+outDir = r'C:\Users\KSherrill\OneDrive - DOI\SFAN\Climate\VulnerabilityAssessment\AETDeficit'  # Directory Output Location
 workspace = f'{outDir}\\workspace'  # Workspace Output Directory
 dateNow = datetime.now().strftime('%Y%m%d')
 logFileName = f'{workspace}\\{outName}_{dateNow}.LogFile.txt'  # Name of the .txt script logfile which is saved in the workspace directory
