b943276369b219bc7c5d0c5a14bed0eab4f1c6f0
krsherrill <kirk_Sherrill@nps.gov>
2024-05-30 15:32:18 -0600

    Create graphAETDeficit.py
    Initial Push

---
+++ graphAETDeficit.py
@@ -0,0 +1,16 @@
+"""
+graphAETDeficitPCM.py
+
+For extracted AET, and Deficit data creates summary graphs
+Input:
+   Point file with extracted Monitoring Locations and Water Balance Data
+Output:
+    1) PDF Figures of AET by Deficit
+
+
+Python Environment: PCM_VegClimateVA - Python 3.11
+Libraries:
+
+Date Developed - May 2024
+Created By - Kirk Sherrill - Data Scientist/Manager San Francisco Bay Area Network Inventory and Monitoring
+"""
