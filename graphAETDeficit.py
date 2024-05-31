e32b6887d73889037f05c92afc98402361291910
krsherrill <kirk_Sherrill@nps.gov>
2024-05-30 22:56:31 -0600

    Update graphAETDeficit.py
    Push workflow for loop by vegetation type and temporal period, scatter plot via MatplotLib and Seaborn - function pointGraphs

--- graphAETDeficit.py
+++ graphAETDeficit.py
@@ -2,6 +2,10 @@
 graphAETDeficitPCM.py
 
 For extracted AET, and Deficit data creates summary graphs
+By Vegetation Type (Iterate):
+1) Graphs AET/Deficit Points: Historic, Futures, and Historic and Futures
+2) Graphs AET/Deficit vectors: Historic, Futures, and Historic and Futures
+
 Input:
    Point file with extracted Monitoring Locations and Water Balance Data
 Output:
@@ -16,16 +20,26 @@ Created By - Kirk Sherrill - Data Scientist/Manager San Francisco Bay Area Netwo
 """
 
 import pandas as pd
-import numpy as np
 import sys
 import os
-import session_info
 import traceback
 from datetime import datetime
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #Excel file with the monitoring Locations
 inPointsWB = r'C:\Users\KSherrill\OneDrive - DOI\SFAN\Climate\VulnerabilityAssessment\AETDeficit\PCM_AETDeficit_20240530.csv'
+
+#Define the dictionary with the Vegetation Type (i.e. Codes), Vegation Names, AET Fields, and Deficit fields to process
+processDic = {'VegType': ["ANGR", "BLUO", "CHRT", "CLOW", "DEPR", "DGLF", "DUNE", "FRSH", "REDW", "SALT", "SCRB",
+                          "SSCR"],
+              'VegName': ["California Annual Grassland", "Blue Oak Woodland", "Bald Hills Prairie",
+                          "Coast Live Oak Woodlands", "Coastal Terrace Prairie", "Douglas Fir Forest",
+                          "Coastal Dune Scrub", "Freshwater Wetlands", "Redwood Forest", "Coastal Salt Marsh",
+                          "Northern Coastal Scrub", "Southern Coastal Scrub"],
+              'Temporal': ["Historic (1981-2010)", "Mid Century (2040-2059)"],
+              'AETFields': ["AET_Historic", "AET_MidCentury"],
+              'DeficitFields': ["Deficit_Historic", "Deficit_MidCentury"]}
 
 
 # Output Name, OutDir, and Workspace
@@ -38,9 +52,27 @@ logFileName = f'{workspace}\\{outName}_{dateNow}.LogFile.txt'  # Name of the .tx
 
 def main():
     try:
-        #Iterate Through the Vegtatation Types - Graphs with Historic, Futures, and Hisoric and Futures
-        #Consider Vector as well mimic Mike's PLOS on AET/Deficit Vectors.
-        
+
+        inFormat = os.path.splitext(inPointsWB)[1]
+        if inFormat == '.xlsx':
+            pointsDF = pd.read_excel(inPointsWB)
+        elif inFormat == '.csv':
+            pointsDF = pd.read_csv(inPointsWB)
+
+        #Convert the Process Dictionary to a Dataframe
+
+
+        #########################################################
+        # Import and Compile the Point Tables (Monitoring Loc and GBIF)
+        #########################################################
+        outFun = pointGraphs(pointsDF, processDic, outDir)
+        if outFun[0].lower() != "success function":
+            messageTime = timeFun()
+            print("WARNING - Function pointGraphs - " + messageTime + " - Failed - Exiting Script")
+            exit()
+
+        outPointsDF = outFun[1]
+
 
 
 
@@ -60,6 +92,102 @@ def main():
         traceback.print_exc(file=sys.stdout)
 
     finally:
+        exit()
+
+
+def pointGraphs(pointsDF, processDic, outDir):
+
+    """
+    Creates AET/Deficit scatter plots by vegetation type (e.g. Vegetation Type
+
+    :param pointsDF: points dataframe to be processed
+    :param processDic: Dictionary defining the Veg Types, Names and AET and Deficit Fields
+    :param outDir: Output directory
+
+    :return: PDFs file with scatter plots of AET/Deficit per Veg Type (i.e. community) at Historica and
+    Futures time steps.
+    """
+    try:
+
+        # Create the dataframe of Veg Types to iterate through
+        veg_type = processDic['VegType']
+        veg_name = processDic['VegName']
+
+        # Create a DataFrame with the Vegetation Types will be outer loop
+        vegTypesDF = pd.DataFrame({'VegType': veg_type, 'VegName': veg_name})
+
+        # Create the dataframe of Veg Types to iterate through
+        temporalFields = processDic['Temporal']
+        aetFields = processDic['AETFields']
+        deficitFields = processDic['DeficitFields']
+
+        # Create a DataFrame defining the temporal information AET and DEficit Fields
+        # will be the inter temporal loop - once historic, once futures
+        temporalDF = pd.DataFrame({'TemporalFields': temporalFields, 'AETFields': aetFields,
+                                   'DeficitFields': deficitFields})
+
+
+        ###STOPPED Here 5/30 - PM
+
+
+
+
+
+        #Remove Records with Negative AET or Deficit
+        filteredDF = pointsDF[(pointsDF['AET_Historic'] >= 0) & (pointsDF['AET_MidCentury'] >= 0)]
+
+        #Subset to all other than 'PCM' records
+        notPCMDF = filteredDF[filteredDF['Source'] != 'PCM']
+
+        #Subset only PCM records
+        onlyPCMDF = filteredDF[filteredDF['Source'] == 'PCM']
+
+        #Define the Style Mappings
+        #style_mapping = {'PCM': 'o', 'GBIF': 's', 'Other': 'X'}
+        size_mapping = {'PCM': 50, 'GBIF': 10, 'Other': 10}
+        color_mapping = {'PCM': '#1f77b4', 'GBIF': '#ff7f0e', 'Other': '#2ca02c'}
+
+        #Define the Color Palette
+        #sns.color_palette("flare", as_cmap=True)
+
+        #Define the scatter Plot Size
+        plt.figure(figsize=(10, 6))
+        # Create the scatter plot with the GBIF (high number of points)
+        sns.scatterplot(data=notPCMDF, x='Deficit_Historic', y='AET_Historic', hue='Source', size='Source',
+                        sizes=size_mapping, palette=color_mapping)
+
+        #Overlay the PCM Plots
+        sns.scatterplot(data=onlyPCMDF, x='Deficit_Historic', y='AET_Historic', hue='Source', size='Source',
+                        sizes=size_mapping, palette=color_mapping)
+
+        plt.xlabel('Avg. Total Annual AET (mm)')
+        plt.ylabel('Avg. Total Annual Deficit (mm)')
+        plt.title('Scatter Plot of AET Historic vs AET MidCentury Grouped by Source')
+
+        # Show plot
+        plt.legend(title='Source')
+
+        outPDF = "TBD"
+        outPath = f'{outDir}\\{outName}.pdf'
+        #Export Plot
+        plt.savefig('scatter_plot.pdf', format='pdf')
+
+        #Close Plot
+        plt.close()
+
+
+        messageTime = timeFun()
+        scriptMsg = f'Successfully preprocessed and merged Locations and GBIF tables - {messageTime}'
+        print(scriptMsg)
+
+        logFile = open(logFileName, "a")
+        logFile.write(scriptMsg + "\n")
+        logFile.close()
+
+        return 'success function', outPointsDF
+
+    except:
+        print(f'Failed - pointsGraphs')
         exit()
 
 
